42dd2289f5f3db03dcaacb8f83c2386934ff8a13
Jory Pratt <anarchy@gentoo.org>
2025-06-07 23:37:32 -0500

    Revert "more voter modifications"
    This reverts commit 0dddc9cde732069e678910cfe5cab863a35b10f5.

--- voterserver.php
+++ voterserver.php
@@ -1,76 +1,174 @@
 <?php
-
-@set_time_limit(0);
-
-include("session.inc");
-session_write_close();
-
-if (!isset($_GET['node']) || !is_numeric($_GET['node'])) {
-    exit();
-}
-$node = (int)$_GET['node'];
+include_once("session.inc");
 
 header('Content-Type: text/event-stream');
 header('Cache-Control: no-cache');
-header('Connection: keep-alive');
+header('X-Accel-Buffering: no');
+date_default_timezone_set('America/Los_Angeles');
+
+include_once('nodeinfo.inc');
+include_once("user_files/global.inc");
+include_once("common.inc");
+include_once("authini.php");
+include_once('amifunctions.inc');
+
+if (empty($_GET['node'])) {
+    echo "data: [FATAL] 'node' parameter is missing.\n\n";
+    ob_flush(); flush(); exit;
+}
+$node = trim(strip_tags($_GET['node']));
+
+$db = $ASTDB_TXT;
+$astdb = [];
+if (file_exists($db)) {
+    $fh = fopen($db, "r");
+    if ($fh && flock($fh, LOCK_SH)) {
+        while (($line = fgets($fh)) !== false) {
+            $trimmed_line = trim($line);
+            if (empty($trimmed_line)) continue;
+            $arr = explode("|", $trimmed_line);
+            if (isset($arr[0])) $astdb[$arr[0]] = $arr;
+        }
+        flock($fh, LOCK_UN); fclose($fh);
+    }
+}
+
+if (!isset($_SESSION['user'])) {
+    echo "data: [FATAL] User session not found. Please log in again.\n\n";
+    ob_flush(); flush(); exit;
+}
+
+$SUPINI = get_ini_name($_SESSION['user']);
+if (!file_exists($SUPINI)) {
+    echo "data: [FATAL] Couldn't load INI file: $SUPINI\n\n";
+    ob_flush(); flush(); exit;
+}
+
+$config = parse_ini_file($SUPINI, true);
+if (!isset($config[$node])) {
+    echo "data: [FATAL] Configuration for node '$node' not found in $SUPINI.\n\n";
+    ob_flush(); flush(); exit;
+}
+$nodeConfig = $config[$node];
+
+$fp = SimpleAmiClient::connect($nodeConfig['host']);
+if ($fp === false) {
+    echo "data: [FATAL] Could not connect to Asterisk Manager on host: {$nodeConfig['host']}.\n\n";
+    ob_flush(); flush(); exit;
+}
+
+if (SimpleAmiClient::login($fp, $nodeConfig['user'], $nodeConfig['passwd']) === false) {
+    echo "data: [FATAL] Could not login to Asterisk Manager. Check user/password.\n\n";
+    ob_flush(); flush();
+    SimpleAmiClient::logoff($fp);
+    exit;
+}
+
+$spinChars = ['*', '|', '/', '-', '\\'];
+$spinIndex = 0;
+$actionIDBase = "voter" . preg_replace('/[^a-zA-Z0-9]/', '', $node);
 
 while (true) {
-    $voter_data = get_voter_data($node);
-    $html_output = format_data_as_html($voter_data, $node);
-
-    echo "id: " . time() . "\n";
-    echo "data: " . $html_output . "\n\n";
-
-    if (ob_get_level() > 0) {
-        ob_flush();
+    $actionID = $actionIDBase . mt_rand(1000, 9999);
+    $response = get_voter_status($fp, $actionID);
+    if ($response === false) {
+        echo "data: Error getting voter response or disconnected.\n\n";
+        ob_flush(); flush();
+        SimpleAmiClient::logoff($fp);
+        exit;
     }
+    
+    $lines = explode("\n", $response);
+    $parsed_nodes_data = [];
+    $parsed_voted_data = [];
+    $currentNodeContext = null;
+    foreach ($lines as $line) {
+        $line = trim($line);
+        if (empty($line)) continue;
+        $parts = explode(": ", $line, 2);
+        if (count($parts) < 2) continue;
+        list($key, $value) = $parts;
+        $$key = $value;
+        if ($key == "Node") {
+            $currentNodeContext = $value;
+            $parsed_nodes_data[$currentNodeContext] = [];
+        }
+        if ($key == "RSSI" && $currentNodeContext && isset($Client)) {
+            $parsed_nodes_data[$currentNodeContext][$Client]['rssi'] = isset($RSSI) ? $RSSI : 'N/A';
+            $parsed_nodes_data[$currentNodeContext][$Client]['ip'] = isset($IP) ? $IP : 'N/A';
+        }
+        if ($key == 'Voted' && $currentNodeContext) {
+            $parsed_voted_data[$currentNodeContext] = $value;
+        }
+    }
+    
+    $message = printNode($node, $parsed_nodes_data, $parsed_voted_data, $nodeConfig);
+    $ticToc = $spinChars[$spinIndex];
+    $spinIndex = ($spinIndex + 1) % count($spinChars);
+
+    echo "data: $message\n";
+    echo "data: $ticToc\n\n";
+    ob_flush();
     flush();
-
-    if (connection_aborted()) {
-        break;
-    }
-
-    sleep(2);
-}
-
-function get_voter_data($node_id) {
-    $signal_strength = rand(0, 255);
-    $station_types = ['voting', 'voted', 'non-voting'];
-    $type = $station_types[array_rand($station_types)];
-
-    return [
-        'signal' => $signal_strength,
-        'type' => $type,
-        'last_updated' => date('H:i:s')
-    ];
-}
-
-function format_data_as_html($data, $node_id) {
-    $signal = htmlspecialchars($data['signal'], ENT_QUOTES, 'UTF-8');
-    $type = htmlspecialchars($data['type'], ENT_QUOTES, 'UTF-8');
-    $time = htmlspecialchars($data['last_updated'], ENT_QUOTES, 'UTF-8');
-    $node_id_safe = htmlspecialchars($node_id, ENT_QUOTES, 'UTF-8');
     
-    $color = '#0099FF';
-    if ($type === 'voted') {
-        $color = 'greenyellow';
-    } else if ($type === 'non-voting') {
-        $color = 'cyan';
-    }
-
-    $html = "
-        <div style='padding: 5px; border: 1px solid #ccc; margin-bottom: 10px;'>
-            <strong>Node: {$node_id_safe}</strong> (Last updated: {$time})<br/>
-            <div style='background-color: #ddd; border-radius: 5px; padding: 2px;'>
-                <div style='width: {$signal}px; max-width: 255px; background-color: {$color}; color: black; text-align: right; padding-right: 5px; border-radius: 3px; min-height: 20px; line-height: 20px;'>
-                    {$signal}
-                </div>
-            </div>
-            <em>Type: {$type}</em>
-        </div>
-    ";
-    
-    return str_replace("\n", "", $html);
+    usleep(150000);
 }
 
+SimpleAmiClient::logoff($fp);
+exit;
+
+function printNode($nodeNum, $nodesData, $votedData, $currentConfig) {
+    global $fp;
+    $message = '';
+    $info = getAstInfo($fp, $nodeNum); 
+
+    if (!empty($currentConfig['hideNodeURL'])) {
+        $message .= "<table class='rtcm'><tr><th colspan=2><i>   Node $nodeNum - $info   </i></th></tr>";
+    } else {
+        $nodeURL = "http://stats.allstarlink.org/nodeinfo.cgi?node=$nodeNum";
+        $message .= "<table class='rtcm'><tr><th colspan=2><i>   Node <a href=\"$nodeURL\" target=\"_blank\">$nodeNum</a> - $info   </i></th></tr>";
+    }
+    $message .= "<tr><th>Client</th><th>RSSI</th></tr>";
+
+    if (!isset($nodesData[$nodeNum]) || empty($nodesData[$nodeNum])) {
+        $message .= "<tr><td><div style='width: 120px;'> No clients </div></td>";
+        $message .= "<td><div style='width: 339px;'> </div></td></tr>";
+    } else {
+        $clients = $nodesData[$nodeNum];
+        $votedClient = isset($votedData[$nodeNum]) && $votedData[$nodeNum] !== 'none' ? $votedData[$nodeNum] : null;
+
+        foreach($clients as $clientName => $client) {
+            $rssi = isset($client['rssi']) ? (int)$client['rssi'] : 0;
+            $bar_width_px = round(($rssi / 255) * 300);
+            $bar_width_px = ($rssi == 0) ? 3 : max(1, $bar_width_px);
+            $barcolor = "#0099FF"; $textcolor = 'white';
+            if ($votedClient && strpos($clientName, $votedClient) !== false) {
+                $barcolor = 'greenyellow'; $textcolor = 'black';
+            } elseif (strpos($clientName, 'Mix') !== false) {
+                $barcolor = 'cyan'; $textcolor = 'black';
+            }
+            $message .= "<tr>";
+            $message .= "<td><div>" . htmlspecialchars($clientName) . "</div></td>";
+            $message .= "<td><div class='text'> <div class='barbox_a'>";
+            $message .= "<div class='bar' style='width: " . $bar_width_px . "px; background-color: $barcolor; color: $textcolor'>" . $rssi . "</div>";
+            $message .= "</div></td></tr>";
+        }
+    }
+    $message .= "<tr><td colspan=2> </td></tr>";
+    $message .= "</table><br/>";
+    
+    return $message;
+}
+
+function get_voter_status($fp, $actionID) {
+    $amiEOL = "\r\n";
+    $action = "Action: VoterStatus" . $amiEOL;
+    $action .= "ActionID: " . $actionID . $amiEOL . $amiEOL;
+
+    if ($fp && fwrite($fp, $action) > 0) {
+        return SimpleAmiClient::getResponse($fp, $actionID);
+    } else {
+        return false;
+    }
+}
 ?>
