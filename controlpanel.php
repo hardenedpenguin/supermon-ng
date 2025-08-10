250fa009ccef2af0421522775b95863a98346657
Jory A. Pratt <geekypenguin@gmail.com>
2025-08-09 23:08:33 -0500

    REFACTOR: Modularized controlpanel.php with comprehensive control system
    🎛️ CONTROL PANEL MODULARIZATION:
    - Reduced controlpanel.php from 159 to 31 lines (81% reduction)
    - Created includes/controlpanel/ directory with 3 specialized modules:
      * controlpanel-auth.inc (98 lines) - Authentication and validation logic
      * controlpanel-ui.inc (137 lines) - HTML, CSS, and JavaScript interface
      * controlpanel-controller.inc (60 lines) - Main orchestration logic
    
    ✅ MAINTAINED FULL FUNCTIONALITY:
    - All authentication and authorization features preserved
    - Node parameter validation and configuration loading maintained
    - AJAX-based command execution functionality intact
    - Dynamic command loading from controlpanel.ini preserved
    - Backward compatibility with existing dependencies (app.js, manifest.json)
    
    🎯 ARCHITECTURE IMPROVEMENTS:
    - Function-based modular design for better maintainability
    - Clear separation of concerns (auth, UI, control)
    - Enhanced security with proper validation functions
    - Improved code organization and readability
    
    🔧 SECURITY FEATURES:
    - CTRLUSER permission validation
    - Node parameter sanitization and validation
    - Configuration file existence checks
    - User session validation

--- controlpanel.php
+++ controlpanel.php
@@ -1,158 +1,31 @@
 <?php
-
+/**
+ * Supermon-ng Control Panel
+ * 
+ * Provides a web-based control panel for managing AllStar nodes.
+ * Allows authenticated users to send commands to specific nodes
+ * through a user-friendly interface.
+ * 
+ * Features:
+ * - User authentication and authorization
+ * - Node-specific command execution
+ * - Dynamic command loading from configuration
+ * - AJAX-based command execution
+ * - Responsive web interface
+ * 
+ * Security: Requires CTRLUSER permission
+ * 
+ * @author Supermon-ng Team
+ * @version 2.0.3
+ * @since 1.0.0
+ */
 
 include("includes/session.inc");
 include("user_files/global.inc");
 include("includes/common.inc");
 include("authusers.php");
 include("authini.php");
+include("includes/controlpanel/controlpanel-controller.inc");
 
-
-if (($_SESSION['sm61loggedin'] !== true) || (!get_user_auth("CTRLUSER"))) {
-    die("<br><h3>ERROR: You Must login to use the 'Control Panel' function!</h3>");
-}
-
-
-$node = @trim(strip_tags($_GET['node']));
-$localnode = @trim(strip_tags($_POST['localnode']));
-
-
-if ($localnode !== '') {
-    $node = $localnode;
-}
-
-
-if (!is_numeric($node)) {
-    die("Please provide a properly formated URI. (ie controlpanel.php?node=1234)");
-}
-
-
-$title = "AllStar $node Control Panel";
-
-
-if ($_SESSION['sm61loggedin'] === true) {
-
-
-    $SUPINI = get_ini_name($_SESSION['user']);
-
-
-    if (!file_exists("$SUPINI")) {
-        die("Couldn't load file $SUPINI.\n");
-    }
-    $allmonConfig = parse_ini_file("$SUPINI", true);
-
-
-    if (!file_exists("$USERFILES/controlpanel.ini")) {
-        die("Couldn't load file controlpanel.ini.\n");
-    }
-    $cpConfig = parse_ini_file("$USERFILES/controlpanel.ini", true);
-
-
-    $cpCommands = $cpConfig['general'];
-
-
-    if (isset($cpConfig[$node])) {
-        foreach ($cpConfig[$node] as $type => $arr) {
-            if ($type == 'labels') {
-                foreach ($arr as $label) {
-                    $cpCommands['labels'][] = $label;
-                }
-            } elseif ($type == 'cmds') {
-                foreach ($arr as $cmd) {
-                    $cpCommands['cmds'][] = $cmd;
-                }
-            }
-        }
-    }
-}
-
-?>
-<!DOCTYPE html>
-<html>
-<head>
-    <title><?php echo $title; ?></title>
-    <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
-    <meta name="generator" content="By hand with a text editor">
-    <meta name="description" content="AllStar Control Panel">
-    <meta name="keywords" content="allstar monitor, app_rpt, asterisk">
-    <meta name="author" content="Tim Sawyer, WD6AWP">
-    <meta name="mods" content="New features, IRLP capability, Paul Aidukas, KN2R">
-    <!-- Modular CSS Files -->
-<link type="text/css" rel="stylesheet" href="css/base.css">
-<link type="text/css" rel="stylesheet" href="css/layout.css">
-<link type="text/css" rel="stylesheet" href="css/menu.css">
-<link type="text/css" rel="stylesheet" href="css/tables.css">
-<link type="text/css" rel="stylesheet" href="css/forms.css">
-<link type="text/css" rel="stylesheet" href="css/widgets.css">
-<link type="text/css" rel="stylesheet" href="css/responsive.css">
-<!-- Custom CSS (load last to override defaults) -->
-<?php if (file_exists('css/custom.css')): ?>
-<link type="text/css" rel="stylesheet" href="css/custom.css">
-<?php endif; ?>
-    <link type="text/css" rel="stylesheet" href="js/jquery-ui.css">
-    <script src="js/jquery.min.js"></script>
-    <script src="js/jquery-ui.min.js"></script>
-
-    <script src="js/sweetalert2.min.js"></script>
-    <link rel="stylesheet" href="js/sweetalert2.min.css"/>
-
-    <script>
-        $(document).ready(function () {
-
-            <?php if ($_SESSION['sm61loggedin'] !== true) { ?>
-            Swal.fire({
-                icon: 'error',
-                title: 'Access Denied',
-                text: 'Must login to use the Control Panel.'
-            });
-
-            <?php } else { ?>
-            $("#cpMain").show();
-
-            <?php } ?>
-
-            $('#cpExecute').click(function () {
-                var localNode = $('#localnode').val();
-                var cpCommand = $('#cpSelect').val();
-
-                $.get('controlserver.php?node=' + localNode + '&cmd=' + cpCommand, function (data) {
-                    alertify.success(data);
-                });
-            });
-        });
-    </script>
-</head>
-<body>
-<div id="header" class="header-dynamic" style="background-image: url(<?php echo $BACKGROUND; ?>); background-color:<?php echo $BACKGROUND_COLOR; ?>; height: <?php echo $BACKGROUND_HEIGHT; ?>;">
-    <div id="headerTitle" class="headerTitle-large"><i><?php echo "$CALL - $TITLE_LOGGED"; ?></i></div>
-    <div id="header4Tag" class="header4Tag-large"><i><?php echo $title ?></i></div>
-    <div id="header2Tag" class="header2Tag-large"><i><?php echo $TITLE3; ?></i></div>
-    <div id="headerImg"><a href="https://www.allstarlink.org" target="_blank"><img src="allstarlink.jpg" width="70%" alt="Allstar Logo"></a></div>
-</div>
-<br>
-<div id="cpMain">
-    <center>Sending Command to node <?php echo $node ?></center>
-    <br>
-    Control command (select one): <select name="cpSelection" class="submit-large" id="cpSelect">
-        <?php
-        for ($i = 0; $i < count($cpCommands['labels']); $i++) {
-            print "<option value=\"" . $cpCommands['cmds'][$i] . "\">" . $cpCommands['labels'][$i] . "</option>\n";
-        }
-        ?>
-    </select>
-    <input type="hidden" id="localnode" value="<?php echo $node ?>">
-    <input type="button" class="submit-large" value="Execute" id="cpExecute">
-    <br/><br>
-    <div id="cpResult">
-        </div>
-</div>
-<br>
-<div class="bottom-left-30">
-    <center>
-        <input type="button" class="submit-large" Value="Close Window" onclick="self.close()">
-        <br><br>
-        <?php include "footer.inc"; ?>
-    </center>
-</div>
-</body>
-</html>
+// Run the control panel system
+runControlPanel();
