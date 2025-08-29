d772defdc852118871f4c9f33826d0b28dda25df
Jory A. Pratt <geekypenguin@gmail.com>
2025-08-29 07:00:00 -0500

    Fix type error in deleteFavorite method
    - Cast index parameter to integer before passing to removeFavoriteFromConfiguration
    - Resolves TypeError: Argument #2 () must be of type string, int given
    - Ensures proper type handling for favorite deletion

--- src/Application/Controllers/ConfigController.php
+++ src/Application/Controllers/ConfigController.php
@@ -2287,6 +2287,9 @@ class ConfigController
                 return $response->withHeader('Content-Type', 'application/json')->withStatus(400);
             }
 
+            // Cast index to integer since the method expects int
+            $index = (int)$index;
+
             $result = $this->removeFavoriteFromConfiguration($currentUser, $section, $index);
             
             $response->getBody()->write(json_encode([
